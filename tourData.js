107929926a541ae6aaac64c949a1f9db956473f6
kjwtuat <99348847+kjwtuat@users.noreply.github.com>
2026-07-22 19:39:13 +0900

    Add files via upload

--- tourData.js
+++ tourData.js
@@ -2,185 +2,129 @@ export const tourData = [
   {
     name: "TYPE0_1",
     type: 0,
-    lat: 35.8891503,
-    lng: 128.6118284,
-    descKo: "",
+    lat: 35.8840698,
+    lng: 128.5958295,
+    descKo: "대구창조경제혁신센터 건물 북문 출입구입니다.",
     descEn: ""
   },
   {
     name: "TYPE0_2",
     type: 0,
-    lat: 35.8891503,
-    lng: 128.6116916,
+    lat: 35.8841219,
+    lng: 128.5956685,
     descKo: "",
     descEn: ""
   },
   {
     name: "TYPE0_3",
     type: 0,
-    lat: 35.8891633,
-    lng: 128.6115468,
+    lat: 35.8840263,
+    lng: 128.595588,
     descKo: "",
     descEn: ""
   },
   {
     name: "TYPE0_4",
     type: 0,
-    lat: 35.889172,
-    lng: 128.6114287,
+    lat: 35.8839177,
+    lng: 128.5954968,
     descKo: "",
     descEn: ""
   },
   {
     name: "TYPE0_5",
     type: 0,
-    lat: 35.8891916,
-    lng: 128.6112893,
+    lat: 35.8838482,
+    lng: 128.5953305,
     descKo: "",
     descEn: ""
   },
   {
     name: "TYPE0_6",
     type: 0,
-    lat: 35.8892003,
-    lng: 128.6111766,
+    lat: 35.883896,
+    lng: 128.5951266,
     descKo: "",
     descEn: ""
   },
   {
     name: "TYPE0_7",
     type: 0,
-    lat: 35.8892176,
-    lng: 128.611072,
-    descKo: "",
+    lat: 35.8839479,
+    lng: 128.5950245,
+    descKo: "CU편의점 앞입니다.",
     descEn: ""
   },
   {
     name: "TYPE0_8",
     type: 0,
-    lat: 35.8892372,
-    lng: 128.6109325,
+    lat: 35.883961,
+    lng: 128.5948931,
     descKo: "",
     descEn: ""
   },
   {
     name: "TYPE0_9",
     type: 0,
-    lat: 35.8892481,
-    lng: 128.610785,
+    lat: 35.8839957,
+    lng: 128.5947563,
     descKo: "",
     descEn: ""
   },
   {
     name: "TYPE0_10",
     type: 0,
-    lat: 35.8892611,
-    lng: 128.6106884,
-    descKo: "",
+    lat: 35.8840087,
+    lng: 128.5946167,
+    descKo: "야외 공연장 부근입니다.",
     descEn: ""
   },
   {
     name: "TYPE0_11",
     type: 0,
-    lat: 35.8891307,
-    lng: 128.6105919,
+    lat: 35.8839371,
+    lng: 128.5945926,
     descKo: "",
     descEn: ""
   },
   {
     name: "TYPE0_12",
     type: 0,
-    lat: 35.8890308,
-    lng: 128.6105704,
+    lat: 35.8838524,
+    lng: 128.5945282,
     descKo: "",
     descEn: ""
   },
   {
     name: "TYPE0_13",
     type: 0,
-    lat: 35.8889222,
-    lng: 128.6105463,
+    lat: 35.883848,
+    lng: 128.5943941,
     descKo: "",
     descEn: ""
   },
   {
     name: "TYPE0_14",
     type: 0,
-    lat: 35.8888114,
-    lng: 128.6105275,
+    lat: 35.8838502,
+    lng: 128.5943136,
     descKo: "",
     descEn: ""
   },
   {
     name: "TYPE0_15",
     type: 0,
-    lat: 35.8887115,
-    lng: 128.6105114,
+    lat: 35.8836395,
+    lng: 128.593989,
     descKo: "",
     descEn: ""
   },
   {
     name: "TYPE0_16",
     type: 0,
-    lat: 35.8886963,
-    lng: 128.6106482,
-    descKo: "",
-    descEn: ""
-  },
-  {
-    name: "TYPE0_17",
-    type: 0,
-    lat: 35.8886789,
-    lng: 128.610785,
-    descKo: "",
-    descEn: ""
-  },
-  {
-    name: "TYPE0_18",
-    type: 0,
-    lat: 35.8886702,
-    lng: 128.6109138,
-    descKo: "",
-    descEn: ""
-  },
-  {
-    name: "TYPE0_19",
-    type: 0,
-    lat: 35.8886506,
-    lng: 128.6110452,
-    descKo: "",
-    descEn: ""
-  },
-  {
-    name: "TYPE0_20",
-    type: 0,
-    lat: 35.8886376,
-    lng: 128.6111981,
-    descKo: "",
-    descEn: ""
-  },
-  {
-    name: "TYPE0_21",
-    type: 0,
-    lat: 35.8886181,
-    lng: 128.6113268,
-    descKo: "",
-    descEn: ""
-  },
-  {
-    name: "TYPE0_22",
-    type: 0,
-    lat: 35.888605,
-    lng: 128.611477,
-    descKo: "",
-    descEn: ""
-  },
-  {
-    name: "TYPE0_23",
-    type: 0,
-    lat: 35.8885898,
-    lng: 128.6116353,
-    descKo: "",
+    lat: 35.8836265,
+    lng: 128.5938092,
+    descKo: "풍국면 가게 앞입니다.",
     descEn: ""
   }
 ];
