3a38ee1f6b495c8f25ed2d7a2c01975363be549a
kjwtuat <99348847+kjwtuat@users.noreply.github.com>
2026-07-22 21:06:48 +0900

    Add files via upload

--- tourData.js
+++ tourData.js
@@ -2,81 +2,81 @@ export const tourData = [
   {
     name: "TYPE0_1",
     type: 0,
-    lat: 35.8840698,
-    lng: 128.5958295,
+    lat: 35.8841606,
+    lng: 128.5958827,
     descKo: "대구창조경제혁신센터 건물 북문 출입구입니다.",
     descEn: ""
   },
   {
     name: "TYPE0_2",
     type: 0,
-    lat: 35.8841219,
-    lng: 128.5956685,
+    lat: 35.8841292,
+    lng: 128.5957177,
     descKo: "",
     descEn: ""
   },
   {
     name: "TYPE0_3",
     type: 0,
-    lat: 35.8840263,
-    lng: 128.595588,
+    lat: 35.8840053,
+    lng: 128.5956211,
     descKo: "",
     descEn: ""
   },
   {
     name: "TYPE0_4",
     type: 0,
-    lat: 35.8839177,
-    lng: 128.5954968,
+    lat: 35.8839412,
+    lng: 128.5954857,
     descKo: "",
     descEn: ""
   },
   {
     name: "TYPE0_5",
     type: 0,
-    lat: 35.8838482,
-    lng: 128.5953305,
+    lat: 35.8839357,
+    lng: 128.5953288,
     descKo: "",
     descEn: ""
   },
   {
     name: "TYPE0_6",
     type: 0,
-    lat: 35.883896,
-    lng: 128.5951266,
+    lat: 35.8839498,
+    lng: 128.5951598,
     descKo: "",
     descEn: ""
   },
   {
     name: "TYPE0_7",
     type: 0,
-    lat: 35.8839479,
-    lng: 128.5950245,
+    lat: 35.883977,
+    lng: 128.5950337,
     descKo: "CU편의점 앞입니다.",
     descEn: ""
   },
   {
     name: "TYPE0_8",
     type: 0,
-    lat: 35.883961,
-    lng: 128.5948931,
+    lat: 35.884002,
+    lng: 128.5948943,
     descKo: "",
     descEn: ""
   },
   {
     name: "TYPE0_9",
     type: 0,
-    lat: 35.8839957,
-    lng: 128.5947563,
+    lat: 35.8840292,
+    lng: 128.5947615,
     descKo: "",
     descEn: ""
   },
   {
     name: "TYPE0_10",
     type: 0,
-    lat: 35.8840087,
-    lng: 128.5946167,
-    descKo: "야외 공연장 부근입니다.",
+    lat: 35.8840324,
+    lng: 128.5946301,
+    descKo: "버거팅 앞 입니다.",
     descEn: ""
   },
   {
@@ -90,41 +90,161 @@ export const tourData = [
   {
     name: "TYPE0_12",
     type: 0,
-    lat: 35.8838524,
-    lng: 128.5945282,
+    lat: 35.8838738,
+    lng: 128.5945214,
     descKo: "",
     descEn: ""
   },
   {
     name: "TYPE0_13",
     type: 0,
-    lat: 35.883848,
-    lng: 128.5943941,
+    lat: 35.8838651,
+    lng: 128.5943846,
     descKo: "",
     descEn: ""
   },
   {
     name: "TYPE0_14",
     type: 0,
-    lat: 35.8838502,
-    lng: 128.5943136,
+    lat: 35.8839292,
+    lng: 128.5942881,
     descKo: "",
     descEn: ""
   },
   {
     name: "TYPE0_15",
     type: 0,
-    lat: 35.8836395,
-    lng: 128.593989,
+    lat: 35.88365,
+    lng: 128.5940628,
     descKo: "",
     descEn: ""
   },
   {
     name: "TYPE0_16",
     type: 0,
-    lat: 35.8836265,
-    lng: 128.5938092,
+    lat: 35.8836413,
+    lng: 128.5938683,
     descKo: "풍국면 가게 앞입니다.",
+    descEn: ""
+  },
+  {
+    name: "TYPE0_17",
+    type: 0,
+    lat: 35.8838252,
+    lng: 128.5958898,
+    descKo: "대구창조경제혁신센터 남측 출입문",
+    descEn: ""
+  },
+  {
+    name: "TYPE0_18",
+    type: 0,
+    lat: 35.8837828,
+    lng: 128.59604,
+    descKo: "",
+    descEn: ""
+  },
+  {
+    name: "TYPE0_19",
+    type: 0,
+    lat: 35.8837546,
+    lng: 128.5961849,
+    descKo: "",
+    descEn: ""
+  },
+  {
+    name: "TYPE0_20",
+    type: 0,
+    lat: 35.8837231,
+    lng: 128.5963351,
+    descKo: "",
+    descEn: ""
+  },
+  {
+    name: "TYPE0_21",
+    type: 0,
+    lat: 35.8836905,
+    lng: 128.5964773,
+    descKo: "",
+    descEn: ""
+  },
+  {
+    name: "TYPE0_22",
+    type: 0,
+    lat: 35.8838556,
+    lng: 128.5957141,
+    descKo: "",
+    descEn: ""
+  },
+  {
+    name: "TYPE0_23",
+    type: 0,
+    lat: 35.8838925,
+    lng: 128.5955893,
+    descKo: "",
+    descEn: ""
+  },
+  {
+    name: "TYPE0_24",
+    type: 0,
+    lat: 35.8835982,
+    lng: 128.5953385,
+    descKo: "카페 동이 출입문 앞",
+    descEn: ""
+  },
+  {
+    name: "TYPE0_25",
+    type: 0,
+    lat: 35.8840076,
+    lng: 128.5941514,
+    descKo: "굴뚝 앞 남측",
+    descEn: ""
+  },
+  {
+    name: "TYPE0_26",
+    type: 0,
+    lat: 35.8841108,
+    lng: 128.5941916,
+    descKo: "굴뚝 앞 북측",
+    descEn: ""
+  },
+  {
+    name: "TYPE0_27",
+    type: 0,
+    lat: 35.883274,
+    lng: 128.5937007,
+    descKo: "이병철 회장 동상 앞",
+    descEn: ""
+  },
+  {
+    name: "TYPE0_28",
+    type: 0,
+    lat: 35.8832469,
+    lng: 128.5935371,
+    descKo: "삼성상회 앞",
+    descEn: ""
+  },
+  {
+    name: "TYPE0_29",
+    type: 0,
+    lat: 35.8835255,
+    lng: 128.5946249,
+    descKo: "메가 커피 앞",
+    descEn: ""
+  },
+  {
+    name: "TYPE0_30",
+    type: 0,
+    lat: 35.8841284,
+    lng: 128.5953627,
+    descKo: "벤처 오피스 서쪽 입구 앞",
+    descEn: ""
+  },
+  {
+    name: "TYPE0_31",
+    type: 0,
+    lat: 35.8838156,
+    lng: 128.5947296,
+    descKo: "버거킹 출입문 앞 입니다.",
     descEn: ""
   }
 ];
